4d8aa000bd61cf4f86f9e6a3fa607b8b1638d020
HarshalNerpagar <harshalnerpagar333@gmail.com>
2024-02-28 16:30:06 +0530

    dataTypes added

---
+++ 2_DataTypes.js
@@ -0,0 +1,15 @@
+/*
+          DataTypes
+ */
+
+let name = "xyz"
+let age = 42
+let isGraterThan_50 = false
+
+// IMP
+
+let todayWeather = null // When we assign any variable with null we get its type as a Object
+let nextKide; // When we create a variable but can't assign any value then it's and undefined type of object
+
+console.log(typeof todayWeather)
+console.log(typeof nextKide)
